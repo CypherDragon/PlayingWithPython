b007eaec1c5530d80e7196b033c415cade7c38b9
CypherDragon <cm.cypherdragon@gmail.com>
2023-04-06 07:44:31 -0500

    Initial commit

---
+++ Python_Crash_Course/Chap4/4-1pizza.py
@@ -0,0 +1,7 @@
+#!/bin/python3
+
+pizzas = ['pizza hut', 'papa johns', 'marcos', 'jet']
+for pizza in pizzas :
+    print(f"You can get a good pizza at {pizza.title()}")
+
+print(f"These are all decent places for pizza!")
---
+++ Python_Crash_Course/Chap4/4-2animals.py
@@ -0,0 +1,7 @@
+#!/bin/python3
+
+animals = ['cat', 'dog', 'hamster', 'guiena pig', 'ferret']
+for animal in animals :
+    print(f"A {animal} would be a good pet")
+
+print("I do love having pets!")
