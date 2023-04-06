abb52d28be025072ca7e44bed0d07c0a6d096cdf
CypherDragon <cm.cypherdragon@gmail.com>
2023-04-06 08:28:17 -0500

    Initial commit

---
+++ Python_Crash_Course/Chap4/4-3-9numberlists.py
@@ -0,0 +1,26 @@
+#!/bin/python3
+
+# 4-3
+for number in range(1,21):
+    print(number)
+
+# 4-4
+numbers = list(range(1,1000001))
+for number in numbers:
+    print(number)
+
+# 4-5
+print(min(numbers))
+print(max(numbers))
+print(sum(numbers))
+
+# 4-6
+odds = list(range(1,21,2))
+for odd in odds:
+    print(odd)
+
+# 4-7
+threes = list(range(3,31,3))
+for three in threes:
+    print(three)
+
